51f8156cff9aa77726bd59f623f33a43241c58b9
maomin <160663534@qq.com>
2021-07-27 00:43:53 +0800

    feat: add robot header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import logo from "./assets/images/logo.svg";
+import styles from "./App.module.css";
 
 import robots from "./mockdata/robots.json";
 import Robot from "./components/Robot";
 
 function App() {
   return (
-    <ul>
-      {robots.map((robot) => (
-        <Robot id={robot.id} name={robot.name} email={robot.email} />
-      ))}
-    </ul>
+    <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt={""} />
+        <h1> 机器人online购物平台 </h1>
+      </div>
+      <div className={styles.robotList}>
+        {robots.map((robot) => (
+          <Robot id={robot.id} name={robot.name} email={robot.email} />
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/components/Robot.tsx
+++ src/components/Robot.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Robot.modules.css";
 
 interface RobotProps {
   id: number;
@@ -8,11 +9,11 @@ interface RobotProps {
 
 const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
   return (
-    <li>
+    <div className={styles.cardContainer} key={id}>
       <img alt="robot" src={`https://robohash.org/${id}`} />
       <h2>{name}</h2>
       <p>{email}</p>
-    </li>
+    </div>
   );
 };
 
